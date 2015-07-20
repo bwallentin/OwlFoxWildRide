8ee23c1e96cd8e59aca73ab5d9220f9bb5a43b3a
Bj?rn Wallentin <Bj?rn Wallentin@localhost>
2015-07-20 14:22:58 +0200

    Beginning of le inventory!

---
+++ Assets/Scripts/Actors/Player/PlayerHealth.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerHealth : Actor
+{
+    private Animator animator;
+    private PlayerMovement playerMovement;
+
+    public override void Awake()
+    {
+        base.Awake();
+        animator = GetComponent<Animator>();
+        playerMovement = GetComponent<PlayerMovement>();
+    }
+
+    void Update()
+    {
+        UpdateHealthBarPosition();
+
+        if (damaged)
+        {
+            // TODO: play damaged animation
+        }
+
+        damaged = false;
+    }
+
+    public void TakeDamage(int amount)
+    {
+        damaged = true;
+
+        currentHealth -= amount;
+        healthSlider.value = currentHealth / (float)startingHealth;
+
+        if (currentHealth <= 0 && !isDead)
+        {
+            Die();
+        }
+    }
+
+    protected override void Die()
+    {
+        isDead = true;
+
+        animator.SetTrigger("Die");
+
+        // Stop moving
+        playerMovement.Stop();
+
+        // disable movement when dead
+        playerMovement.enabled = false;
+    }
+}
