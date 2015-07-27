d293b05c95ed1f24c35cee9666d49318b86034f8
Bj?rn Wallentin <Bj?rn Wallentin@localhost>
2015-07-27 16:18:51 +0200

    Change playerstats

--- Assets/Scripts/Actors/Player/PlayerStats.cs
+++ Assets/Scripts/Actors/Player/PlayerStats.cs
@@ -3,17 +3,41 @@ using System.Collections;
 
 public class PlayerStats : MonoBehaviour, IPlayerStats {
 
-    public int Strength { get; set; }
-    public int Mana { get; set; }
-    
+    public int Armor { get; set; }
+    public int Damage { get; set; }
+    public int Crit { get; set; }
+    public int Dodge { get; set; }
+    public int Hit { get; set; }
+    public double Mana { get; set; }
+    public int Resist { get; set; }
+    public int Speed { get; set; }
+    public int XP { get; set; }
+
+    private double baseMana;
+
     public PlayerStats()
     {
-        Strength = 0;
-        Mana = 100;
+        Armor = 0;
+        Damage = 10;
+        Crit = 0;
+        Dodge = 0;
+        Hit = 100;
+        Mana = CalculateBaseMana();
+        Resist = 0;
+        Speed = 0;
+        XP = 0;
     }
 
-    public void RecalculateMana(int amountManaUsed)
+    public double CalculateBaseMana()
     {
-        Mana -= amountManaUsed;
+        //baseMana = Mana + (Intellect * 0.5);
+        //return baseMana;
+
+        return 100;
+    }
+
+    public void RecalculateMana(int amountOfManaUsed)
+    {
+        Mana -= amountOfManaUsed;
     }
 }
