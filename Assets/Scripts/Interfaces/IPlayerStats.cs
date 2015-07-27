2e2618e23b56a9fae7e7984c86126fca4bc9736b
Bj?rn Wallentin <Bj?rn Wallentin@localhost>
2015-07-27 16:18:51 +0200

    Change playerstats

--- Assets/Scripts/Interfaces/IPlayerStats.cs
+++ Assets/Scripts/Interfaces/IPlayerStats.cs
@@ -3,7 +3,17 @@ using System.Collections;
 
 public interface IPlayerStats {
 
-    int Strength { get; set; }
-    int Mana { get; set; }
+    int Armor { get; set; }
+    int Damage { get; set; }
+    int Crit { get; set; }
+    int Dodge { get; set; }
+    int Hit { get; set; }
+    double Mana { get; set; }
+    int Resist { get; set; }
+    int Speed { get; set; }
+    int XP { get; set; }
 
+    double CalculateBaseMana();
+
+    void RecalculateMana(int amountOfManaUsed);
 }
